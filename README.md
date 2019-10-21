d0f48772096f4b676ea40015c3d4b05587a5b502
Julian Hyde <jhyde@apache.org>
2019-10-20 20:45:05 -0700

    Optimize pythagoras.py, and don't print trivial solutions

--- README.md
+++ README.md
@@ -12,3 +12,24 @@ Adds fractions and convert to lowest terms.
 
 September, 2019 ([code](python_work/factor.py))
 
+## Pythagorean triples
+
+Generate triples (a, b, c) such that a, b, c are positive integers
+and a<sup>2</sup> + b<sup>2</sup> = c<sup>2</sup>.
+
+```
+$ python3 ./pythagoras.py
+   a    b    c
+==== ==== ====
+   3    4    5
+   5   12   13
+   8   15   17
+   7   24   25
+  20   21   29
+  12   35   37
+   9   40   41
+  28   45   53
+  11   60   61
+```
+
+October, 2019 ([code](python_work/pythagoras.py))
--- python_work/pythagoras.py
+++ python_work/pythagoras.py
@@ -2,11 +2,31 @@
 # Each triple has the form (a, b, c) where a^2 + b^2 = c^2
 # and a, b, c are positive integers.
 
-for c in range(3, 25):
-    for b in range(3, 25):
-        for a in range(3, 25):
-            if b >= c or a >= b :
-                continue
-            if a*a + b*b ==c*c:
-                print(f"a={a} b={b} c={c}")
+def gcd(e, f):
+    if e == 0:
+        if f == 0:
+            return 1 # special case: gcd(0, 0) is 1
+        else:
+            return f # special case: gcd(0, x) is x
+    elif e < 0:
+        return gcd(-e, f)
+    elif f < 0:
+        return gcd(e, -f)
+    elif e == f:
+        return e
+    elif e < f:
+        return gcd(e, f - e)
+    else:
+        return gcd(e - f, f)
+
+
+print(f"   a    b    c")
+print(f"==== ==== ====")
+for c in range(5, 1000):
+    for b in range(4, c):
+        for a in range(3, b):
+            if a*a + b*b >= c*c:
+                if a*a + b*b == c*c and gcd(a, b) == 1:
+                    print(f"{a:4} {b:4} {c:4}")
+                break
         	
